b42ecc7bd8101c57fa2504ff6a0f2bc84da97aa8
Manovate <manovate15@gmail.com>
2025-08-29 17:58:22 +0530

    Update create_admin.py

--- super-two/create_admin.py
+++ super-two/create_admin.py
@@ -1,18 +1,17 @@
 # create_admin.py
 from app import app
-from .models import db, Admin
+from models import db, Admin
 from werkzeug.security import generate_password_hash
 
 email = input("Admin email: ").strip().lower()
 pw = input("Admin password: ").strip()
+
 if not email or not pw:
-    print("Provide both")
+    print("❌ Provide both email and password")
 else:
     with app.app_context():
-        db.create_all()
         hashed = generate_password_hash(pw)
-        a = Admin(email=email, password=hashed)
-        db.session.add(a)
+        admin = Admin(email=email, password=hashed)
+        db.session.add(admin)
         db.session.commit()
-        print("Admin created:", email)
-
+        print(f"✅ Admin created: {email}")
